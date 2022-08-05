79419930aedc240593395016c1d23c270eec92a5
aroramrinaal <aroramrinaal@gmail.com>
2022-08-05 23:16:12 +0530

    Lets Learn Java

--- src/TheClassName.java
+++ src/TheClassName.java
@@ -1,9 +1,9 @@
 public class TheClassName {
     public static void theFirstMethodName(){
-
+        // Program Statement.
     }
 
     public static void theSecondMethodName() {
-
+        // Program Statement.
     }
 }

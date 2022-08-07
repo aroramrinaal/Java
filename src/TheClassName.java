6beb7df232bcb999138c5249aca5d12ea5f753bc
aroramrinaal <aroramrinaal@gmail.com>
2022-08-07 15:52:26 +0530

    Lets Learn Java

--- src/TheClassName.java
+++ src/TheClassName.java
@@ -1,10 +1,13 @@
 public class TheClassName {
     public static void theFirstMethod(){
-        String name = "Mrinaal";
-        System.out.println("Hello, "+name);
+
+    }
+    public static void theSecondMethod(){
+
     }
 
     public static void main(String[] args) {
-        theFirstMethod();
+        // variables go here
+        //instructions go here.
     }
 }

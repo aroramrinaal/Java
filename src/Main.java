c0961742c6b301bdfdefa3f070fc738660c5d9aa
aroramrinaal <aroramrinaal@gmail.com>
2022-08-07 22:44:22 +0530

    Lets Learn Java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int result = addTwoNums(3,4);
-        System.out.println(result);
-    }
-
-    public static int addTwoNums(int a,int b ){
-        return a+b;
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
     }
 }

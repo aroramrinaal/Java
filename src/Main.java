f4172b1391e0c357f66409d40e6d06cdfb45cf8d
aroramrinaal <aroramrinaal@gmail.com>
2022-08-07 23:13:40 +0530

    Lets Learn Java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println(input.nextInt());
+        System.out.println("Hello World");
     }
 }

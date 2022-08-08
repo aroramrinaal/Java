96059d7c878169bdaec815cb771ed66a05541a11
aroramrinaal <aroramrinaal@gmail.com>
2022-08-08 19:27:49 +0530

    Lets Learn Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,World!");
+        String firstName = "Mrinaal";
+        int age = 19;
+        double myDouble = 5.99;
     }
 }

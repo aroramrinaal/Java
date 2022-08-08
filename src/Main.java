04c92e110236013d33c2e939a2d227830061f999
aroramrinaal <aroramrinaal@gmail.com>
2022-08-08 11:10:37 +0530

    Lets Learn Java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hey,What's your name?");
-        Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
-        System.out.println("Hello "+name);
+        System.out.println("Hello,World!");
     }
 }

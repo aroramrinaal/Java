15b6c613fa532f0441594b6cd1b7ce7f11c3de22
aroramrinaal <aroramrinaal@gmail.com>
2022-08-23 15:43:01 -0700

    Lets Learn Java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int myNum = 15;
-        float myFloat = 5.987f;
-        boolean myBool = true;
-        String myText = "Mrinaal";
-        char myChar = 'D';
-        System.out.println(myBool);
-        System.out.println(myFloat);
-        System.out.println(myChar);
-        System.out.println(myNum);
-        System.out.println(myText);
+        System.out.println("Hello,World!");
     }
 }

9b0d3b7fbfe59402eec693bc877aef2d4938be4e
aroramrinaal <aroramrinaal@gmail.com>
2022-08-23 15:35:49 -0700

    Lets Learn Java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String name = "Mrinaal";
-        System.out.println(name);
+        int myNum = 15;
+        myNum = 20;
+        System.out.println(myNum);
     }
 }

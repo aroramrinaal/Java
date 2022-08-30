e1b3e788b3416dbe332f3123349cf03b98823fd9
aroramrinaal <aroramrinaal@gmail.com>
2022-08-29 18:40:39 -0700

    Lets Learn Java

---
+++ src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+    public static void main(String[] args) {
+        double radius = 10.25;
+        double diameter = radius*2;
+        double circumference = 2*(3.1415)*radius;
+        double area = (3.1415)*(Math.pow(radius,2));
+        double areaOfSemicircle = (area/2);
+
+
+        System.out.println("Properties of a Circle:");
+
+    }
+}

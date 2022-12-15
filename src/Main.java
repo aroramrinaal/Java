44402c7afb4e9796d7a31c2ced28789738b5d9f3
aroramrinaal <aroramrinaal@gmail.com>
2022-12-15 16:43:43 +0530

    Java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main{
     public static void main(String[] args) {
         // System.out.println("Hello World");
-        int number = 23;
-        System.out.println(number);
+        // int number = 23;
+        
     }
 }
 

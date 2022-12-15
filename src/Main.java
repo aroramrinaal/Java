54927c1c4c352ad37abc319cdfe3ffcf5fea9d04
aroramrinaal <aroramrinaal@gmail.com>
2022-12-15 17:04:54 +0530

    Java Practice

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-        boolean isMale = true;
-        if(isMale==true){
-            System.out.println("person is male.");
-        }else{
-            System.out.println("person is female.");
-        }
     }
 }
 

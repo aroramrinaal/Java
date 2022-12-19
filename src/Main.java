a8850309dcf602c06381486d136e70e80f02f00c
aroramrinaal <aroramrinaal@gmail.com>
2022-12-19 18:06:47 +0530

    OOPs

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String[] args) {
         Bunny myBunny = new Bunny();
         myBunny.age = 3;
-        myBunny.name = "Flopsy";
+        myBunny.name = "Boomer";
         myBunny.getName();
     }
 }

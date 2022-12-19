6961e2cf1a865ed453a80092716ff77efc2c0842
aroramrinaal <aroramrinaal@gmail.com>
2022-12-19 21:26:53 +0530

    OOPS In Java

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,17 @@
-public class Main{
-    public static void main(String[] args) {
-        Tree myFavoriteOakTree = new Tree(110, 5, "Oak");
-        myFavoriteOakTree.announceTallTree();
-    }
-}
-
-class Tree{
-    double heightFt;
-    double trunkDiameterInches;
-    String treeType;
-
-    Tree(double heightFt,double trunkDiameterInches,String treeType){
-        this.heightFt = heightFt;
-        this.trunkDiameterInches = trunkDiameterInches;
-        this.treeType = treeType;
-    }
-
-    void grow(){
-        this.heightFt = this.heightFt + 10;
-        this.trunkDiameterInches = this.trunkDiameterInches+1;
-    }
-
-    void announceTallTree(){
-        if (this.heightFt>100) {
-            System.out.println("That's a tall "+(String) this.treeType+" tree.");
-        }
-    }
-
-}
-
-
 // Java Practice
 // OOPS In Java
+
+
+
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Employee{
+    String name;
+    int age;
+    double salary;
+    String location;
+}

5ec672e1a3ebe5a07d4fe30d8635b31f58935855
aroramrinaal <aroramrinaal@gmail.com>
2022-12-19 18:53:03 +0530

    OOPS In Java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main{
         Bunny myBunny = new Bunny();
         myBunny.age = 3;
         myBunny.name = "Boomer";
-        myBunny.getName();
     }
 }
 
@@ -11,8 +10,14 @@ class Bunny{
     String name;
     int age;
 
-    void getName(){
-        System.out.println("Name of the bunny is "+name+".");
+    void hop(){
+
+    }
+    void eat(String food){
+
+    }
+    void sleep(){
+
     }
 }
 

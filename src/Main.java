6dd0b4ad2b6ad87badd0b0af31ab005591d42b52
aroramrinaal <aroramrinaal@gmail.com>
2022-12-19 21:32:40 +0530

    OOPS In Java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@
 
 public class Main{
     public static void main(String[] args) {
-        
+        Employee employee1 = new Employee("Joe",24,50000,"Tempe");
+        employee1.getEmployeeInfo();
     }
 }
 
@@ -14,4 +15,19 @@ class Employee{
     int age;
     double salary;
     String location;
+
+    Employee(String name,int age,double salary,String location){
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.location = location;
+    }
+
+    void raiseSalary(){
+        this.salary = this.salary+10000;
+    }
+
+    void getEmployeeInfo(){
+        System.out.println(name+" is "+age+" years old.He works in "+location+" for a salary of "+salary+".");
+    }
 }

daa5b198f9280064d6c06ba11cc0cbff1128ace2
aroramrinaal <aroramrinaal@gmail.com>
2022-12-19 14:57:49 +0530

    Java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 public class Main{
     public static void main(String[] args) {
-        
+        Bunny myBunny = new Bunny();
+        myBunny.age = 3;
+        myBunny.name = "Flopsy";
+        myBunny.getName();
     }
 }
 
+class Bunny{
+    String name;
+    int age;
+
+    void getName(){
+        System.out.println("Name of the bunny is "+name+".");
+    }
+}
 
 // Java Practice
 // OOPS In Java

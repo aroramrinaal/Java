eca0b16867dfa45730923ba676d1debc75e9cce8
aroramrinaal <aroramrinaal@gmail.com>
2022-12-20 12:29:21 +0530

    OOPS In Java

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,3 @@
 // OOPS In Java
 
 
-
-public class Main{
-    public static void main(String[] args) {
-        Employee employee1 = new Employee("Joe",24,50000,"Tempe");
-        employee1.getEmployeeInfo();
-    }
-}
-
-class Employee{
-    String name;
-    int age;
-    double salary;
-    String location;
-
-    Employee(String name,int age,double salary,String location){
-        this.name = name;
-        this.age = age;
-        this.salary = salary;
-        this.location = location;
-    }
-
-    void raiseSalary(){
-        this.salary = this.salary+10000;
-    }
-
-    void getEmployeeInfo(){
-        System.out.println(name+" is "+age+" years old.He works in "+location+" for a salary of "+salary+".");
-    }
-}

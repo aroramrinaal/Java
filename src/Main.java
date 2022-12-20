2267e498fd265f47b3ec72fbd8615bcc845a51d2
aroramrinaal <aroramrinaal@gmail.com>
2022-12-20 13:55:16 +0530

    OOPS In Java

--- src/Main.java
+++ src/Main.java
@@ -2,3 +2,19 @@
 // OOPS In Java
 
 
+public class Main{
+
+}
+
+class Employee{
+    String name;
+    int age;
+    int salary;
+    String location;
+
+    Employee(String name,int age,int salary,String location){
+        this.name = name;
+        this.age = age;
+
+    }
+}

8dc7328e1f4077fdcd80afc93bfd3f679fb2a556
aroramrinaal <aroramrinaal@gmail.com>
2022-12-21 13:10:41 +0530

    OOPS In Java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,2 @@
 // Java Practice
 // OOPS In Java
-
-public class Main{
-    public static void main(String[] args) {
-    }
-}
-
-class BankAccount{
-    private String owner;
-    private double balance;
-
-    public String getOwner(){
-        return owner;
-    }
-
-    public double getBalance(){
-        return balance;
-    }
-
-    BankAccount(String owner,double balance){
-        this.owner = owner;
-        this.balance = balance;
-    }
-}

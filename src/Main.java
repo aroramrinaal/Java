a3d8a73c9d53c7c9f12ded8df9740626e4e6e410
aroramrinaal <aroramrinaal@gmail.com>
2022-12-21 17:13:46 +0530

    OOPS In Java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
 // Java Practice
 // OOPS In Java
+
+public class Main{
+    public static void main(String[] args) {
+        BabyDog 
+    }
+}
+
+class Animal{
+    void eating(){
+        System.out.println("eating...");
+    }
+}
+
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("barking...");
+    }  
+}
+
+
+class BabyDog extends Dog{
+    void weeps(){
+        System.out.println("weeping...");
+    }
+}

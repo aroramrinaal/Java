c4806fd218eb396bc717af793d7b0dab21e60d78
aroramrinaal <aroramrinaal@gmail.com>
2022-12-26 13:51:07 +0530

    OOPS In Java

--- src/practice.java
+++ src/practice.java
@@ -1,7 +1,5 @@
 public class practice {
     public static void main(String[] args) {
-        int count;
-        count = 19;
-        System.out.println(count);
+        // char bool int float double primitive types
     }
 }

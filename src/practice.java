850e083f6da415ab2f1f7c5f6fc7e27859dafb62
aroramrinaal <aroramrinaal@gmail.com>
2022-12-26 16:14:34 +0530

    OOPS In Java

--- src/practice.java
+++ src/practice.java
@@ -1,5 +1,25 @@
 public class practice {
     public static void main(String[] args) {
-        // 
+        Engineer engineer1 = new Engineer();
+        engineer1.name = "Mrinaal";
+        System.out.println(engineer1.name);
     }
+}
+
+
+
+class Employee{
+    int employeeId;
+    String name;
+    int age;
+    double salary;
+}
+
+
+class Engineer extends Employee{
+    double bonus = 10000+salary;
+}
+
+class Manager extends Employee{
+    double bonus = 20000+salary;
 }
